fa1988311f69666a4f07dff95769c887121f2a8f
Imad Ahmed <imadahmed101@gmail.com>
2023-10-08 19:38:25 -0400

    added routes to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='/education' element={<Education />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/community' element={<Community />} />
+        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/post/:postID' element={<Post />} />
+        <Route path='/editprofile' element={<EditProfile />} />
+        <Route path='/editeducation' element={<EditEducation />} />
+        <Route path='/editwork' element={<EditWork />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
   );
 }
 
