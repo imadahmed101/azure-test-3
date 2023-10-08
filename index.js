09cda745bd830301a85f75dfd01a9d4c450a7628
Imad Ahmed <imadahmed101@gmail.com>
2023-10-07 20:47:10 -0400

    updated index server file

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const path = require('path')
 const port = process.env.PORT || 80
 
 // app.use(express.static("./client/build"))
-app.use('/',express.static(path.join(__dirname, 'client', 'build', "index.html")));
+// app.use('/',express.static(path.join(__dirname, 'client', 'build', "index.html")));
+app.use(express.static('client/build'));
 app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res.send('home page accessed')
 // })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
 
 app.post('/login', (req, res) => {
     const username = req.body.username
