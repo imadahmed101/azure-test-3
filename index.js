bce709060b90c579c3560247bc43f65544628500
Imad Ahmed <imadahmed101@gmail.com>
2023-10-07 20:05:35 -0400

    updated index server files to include express.static and app.get with path.resolve

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app =  express()
 const port = process.env.PORT || 80
 
+app.use(express.static("./client/build"))
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('home page accessed')
-})
+// app.get('/', (req, res) => {
+//     res.send('home page accessed')
+// })
 
 app.post('/login', (req, res) => {
     const username = req.body.username
@@ -14,6 +15,10 @@ app.post('/login', (req, res) => {
 
     res.json({username, password})
 })
+
+app.get("/", (res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log('server running')
